c4598865272df858454a2a1a3c3232fa7ad55b06
KelanRaff <kelanrafferty@gmail.com>
2024-01-05 15:27:52 +0000

    CSV data reader implemented

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import data.DataReader;
+import data.Image;
+
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+        List<Image> images = new DataReader().readData("data/mnist_test.csv");
+        System.out.printf(images.get(0).toString());
     }
 }
